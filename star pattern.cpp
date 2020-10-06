df69a9cfece3cb7fd576fbbd528dd185e9dd6ffe
MRUNAL-SADAWARTE <71922775+MRUNAL-SADAWARTE@users.noreply.github.com>
2020-10-06 22:38:41 +0530

    drawing patterns in c++(day 20

---
+++ star pattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int i,j,k;
+	for (i=11;i>=1;i--)
+	{
+		for(j=1;j<=i;j++)
+		{
+		    cout<<" ";
+	    }
+	    for(k=11;k>=i;k--)
+
+	    {
+		    cout<<"*";
+	    }
+        cout<<"\n";
+    }
+}
+
+	
