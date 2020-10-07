d9b95e740d470a2f9aacd187247f498ccb30c9af
MRUNAL-SADAWARTE <71922775+MRUNAL-SADAWARTE@users.noreply.github.com>
2020-10-07 22:53:11 +0530

    basics

---
+++ basics.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1,num2,res,choise;
+	char op;
+	char c;
+	do
+	{
+	system("CLS");
+	cout<<"\nenter first number :";
+	cin>>num1;
+	cout<<"\nenter second number :";
+	cin>>num2;
+	cout<<"\nmenu";
+	cout<<"\n1. series of odd numbers";
+	cout<<"\n2. series of prime numbers";
+	cout<<"\n3. enter your choise:";
+	
+	
+	switch (choise)
+	{
+		case1: 
+		for( int num1=1,num<=2* n: i++)
+		    if (i%2!=0)
+		    cout<<i<<" ";
+		    break;
+		    
+		case2:
+		for ( i=2;i<=n/2; ++i)
+		     if(n%1==0){
+		     	is prime=false;
+		    break;
+			 }
+	
+		
+	}
+	
+	}
+
+}
